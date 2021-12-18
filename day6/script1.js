965a29a5cc093c3332dd30d3f360a4875298137d
Sebastian Green <krewllobster@gmail.com>
2021-12-18 13:13:12 -0500

    day 6 part 1

--- day6/script1.js
+++ day6/script1.js
@@ -1,3 +1,23 @@
 const fs = require("fs");
 const data = fs.readFileSync("./input.txt");
-const rows = data.toString().split("\n");
+const initialFish = data
+  .toString()
+  .split(",")
+  .map((x) => parseInt(x, 10));
+
+const fishReady = (fish) => fish === 0;
+const ageFish = (age) => (age === 0 ? 6 : age - 1);
+
+const passDay = (currentFish) => {
+  const newFish = currentFish.filter(fishReady).map((_) => 8);
+  const agedFish = currentFish.map(ageFish);
+  return [...agedFish, ...newFish];
+};
+
+let day = 1;
+let currentFish = initialFish;
+while (day <= 80) {
+  currentFish = passDay(currentFish);
+  day += 1;
+}
+console.log(currentFish.length);
