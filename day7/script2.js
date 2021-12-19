efe9452c8eb6b4a89d6cf1e1e9e4325591f2699c
Sebastian Green <krewllobster@gmail.com>
2021-12-18 23:18:26 -0500

    day 7 part 2

--- day7/script2.js
+++ day7/script2.js
@@ -1,2 +1,49 @@
 const fs = require("fs");
 const data = fs.readFileSync("./input.txt");
+const initialData = data
+  .toString()
+  .split(",")
+  .map((x) => parseInt(x, 10))
+  .sort((a, b) => a - b);
+
+const initialPositions = new Map();
+
+for (const pos of initialData) {
+  if (initialPositions.has(pos)) {
+    initialPositions.set(pos, initialPositions.get(pos) + 1);
+  } else {
+    initialPositions.set(pos, 1);
+  }
+}
+
+const calculateFuel = (count, pos, target) => {
+  let dist = Math.abs(pos - target);
+  let cost = 0;
+  for (let i = 1; i <= dist; i++) {
+    cost += count * i;
+  }
+  return cost;
+};
+
+const getFuelCost = (positions, target) => {
+  let fuelCost = 0;
+  for (const [pos, count] of positions.entries()) {
+    fuelCost += calculateFuel(count, pos, target);
+  }
+  return fuelCost;
+};
+
+console.log(initialPositions);
+
+const minPos = Math.min(...initialPositions.keys());
+const maxPos = Math.max(...initialPositions.keys());
+
+const fuelCosts = [];
+
+let x = minPos;
+
+while (x < maxPos) {
+  fuelCosts.push(getFuelCost(initialPositions, x));
+  x += 1;
+}
+console.log(Math.min(...fuelCosts));
